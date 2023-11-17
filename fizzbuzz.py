b7a66ae5349c3029ba561c59d30b1631f3150285
VeritasInData <veritasindata@gmail.com>
2023-11-17 17:52:35 +0900

    feat: list comprehension

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
-for i in range(1,16+1):
-    if i % 3 == 0:
-        print('fizz')
-    else:
-        print(i)
+result=[
+    'fizzbuzz' if i%15==0 else 
+    'fizz' if i%3==0 else 
+    'buzz' if i%5==0 else
+    i for i in range (1, 16+1)
+]
+print(result)
